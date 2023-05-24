d2fc0f93aca638a51aa06cd6bad884803396d3ff
Robert Grigoryan <rg448242@students.mimuw.edu.pl>
2023-05-24 21:35:04 +0200

    Added comments for BacktrackParameters

--- src/main/scala/BacktrackParameters.scala
+++ src/main/scala/BacktrackParameters.scala
@@ -1,4 +1,10 @@
+/**
+ * Backtrack parameters for finding solutions
+ *
+ * @param maxPathLength we are looking for solutions, whose length does not exceed maxPathLength
+ * @param numberOfTopResultsToOutput number of the shortest results to output
+ */
 case class BacktrackParameters(
   maxPathLength: Int,
-  numberOfResultsToOutput: Int
+  numberOfTopResultsToOutput: Int
 )
